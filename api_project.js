b489264716621a2844296b5431307c11c450d8f9
Fangli Pi <hpcfapix@hlrs.de>
2017-03-29 11:26:53 +0200

    created by apidoc -i /xxx -o /xxx

---
+++ api_project.js
@@ -0,0 +1,32 @@
+define({
+  "name": "PHANTOM Monitoring API",
+  "version": "1.0.0",
+  "description": "RESTful service of the PHANTOM monitoring framework",
+  "title": "PHANTOM Monitoring API",
+  "url": "http://mf.excess-project.eu:3033/v1/phantom_mf",
+  "header": {
+    "title": "PHANTOM Monitoring API"
+  },
+  "order": [
+    "Configs",
+    "Workflows",
+    "Experiments",
+    "Runtime",
+    "Profiles",
+    "Metrics",
+    "Statistics"
+  ],
+  "template": {
+    "withCompare": true,
+    "withGenerator": true
+  },
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2017-03-29T09:24:43.735Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.5"
+  }
+});
